f573fd8aeb1648d1e2fb4a9dc1c2317eab323c9c
xiongtian <18771777589@163.com>
2019-07-15 13:24:33 +0800

    1.2 初始化页面布局

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
+      <Header/>
     <nuxt />
+  
+    <Footer/>
   </div>
 </template>
+
+<script>
+import Header  from "@/components/header"
+import Footer  from "@/components/footer"
+export default {
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
+
 
 <style>
 html {
@@ -17,39 +32,20 @@ html {
   box-sizing: border-box;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
+*{
+  margin:0;
+  padding:0
+}
+ul,li,ol{
+  list-style:none;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+a{
+  color:inherit;
   text-decoration: none;
-  padding: 10px 30px;
+}
+em,i{
+  font-style:normal;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
