260347a9ed448f7038435f5722749e335f36db29
xiongtian <18771777589@163.com>
2019-07-15 14:15:08 +0800

    1.2 Initialize the project structure

--- components/header.vue
+++ components/header.vue
@@ -1,5 +1,27 @@
 <template>
-    <div class="header">头部</div>
+    <div class="header">
+        <!-- logo -->
+        <div>
+            <nuxt-link>
+                   <img src="http://157.122.54.189:9093/images/logo.jpg" alt="">
+            </nuxt-link>
+        </div>
+        <!-- 菜单 -->
+        <div>
+            <nuxt-link to='/'>首页</nuxt-link>
+            <nuxt-link to='/'>旅游攻略</nuxt-link>
+            <nuxt-link to='/'>酒店</nuxt-link>
+            <nuxt-link to='/'>国内机票</nuxt-link>
+        </div>
+        <!-- 登陆信息 -->
+        <div>
+            <div v-if="false">
+                <!-- 注意：跳转的后面需要加to，不然容易报错 -->
+                <nuxt-link to='/user/login'>登录 / 注册</nuxt-link>
+            </div>
+        
+        </div>
+    </div>
 </template>
 
 <script>
